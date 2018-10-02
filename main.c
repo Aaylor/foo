4fa48e73de094b45f2e7757b8c2356acd1d977a4
Loïc Runarvot <loic.runarvot@trust-in-soft.com>
2018-10-02 12:20:00 +0200

    update3

--- main.c
+++ main.c
@@ -1,12 +1,3 @@
-int pow(int x, int n) {
-  int res = 1;
-  while (n > 0) {
-    res *= x;
-    n--;
-  }
-  return res;
-}
-
 int main(int argc, char **argv) {
-  return pow(4, 2);
+  return 21 * 2;
 }
