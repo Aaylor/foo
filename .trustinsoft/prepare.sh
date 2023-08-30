2a968d30c95076dc3e8b0c35bfa7191143c9cfae
Loïc Runarvot <loic.runarvot@trust-in-soft.com>
2023-08-30 11:58:08 +0200

    Update prepare.sh

--- .trustinsoft/prepare.sh
+++ .trustinsoft/prepare.sh
@@ -4,6 +4,8 @@ ls -AR
 which tis-analyzer
 
 export TIS_KERNEL_SHARE="/home/tis/tis-taas-1.43.3/share/tis-kernel"
+export TIS_KERNEL_LIB="/home/tis/tis-taas-1.43.3/lib/tis-kernel"
+export TIS_KERNEL_PLUGIN="/home/tis/tis-taas-1.43.3/lib/tis-kernel/plugins"
 
 tis-kernel -tis-ci-check-config .trustinsoft/config.json || true
 tis-kernel -print-config
